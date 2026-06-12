1522262d28cae562992c67ca42b0ef7ef24501c7
Rahul12jain <jain89512@gmail.com>
2026-06-12 15:56:06 +0530

    improved layout

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -15,27 +15,3 @@ function Layout() {
 }
 
 export default Layout;
-
-// import Navbar from "./Navbar";
-// import Footer from "./Footer";
-// import { Outlet } from "react-router-dom";
-
-// function Layout() {
-//   return (
-//     <div className="min-h-screen flex flex-col">
-//       {/* Top Navbar */}
-//       <Navbar />
-
-//       {/* Page Content */}
-//       <main className="flex-grow p-4">
-//         <Outlet />
-//       </main>
-
-//       {/* Bottom Footer */}
-//       <Footer />
-//     </div>
-//   );
-// }
-
-// export default Layout;
-
